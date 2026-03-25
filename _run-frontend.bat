1423da237e10df699bc9b1d0c8073a73a2f68a1f
AlejandroFloresF <jafg84alejandro@gmail.com>
2026-03-25 17:00:11 -0600

    utils

--- _run-frontend.bat
+++ _run-frontend.bat
@@ -5,9 +5,9 @@ echo  [Frontend] Actualizando repositorio...
 git pull
 echo.
 echo  [Frontend] Instalando dependencias npm...
-npm install --no-audit --no-fund
+call npm install --no-audit --no-fund
 echo.
 echo  [Frontend] Iniciando servidor Vite...
 echo  ---------------------------------------
-npm run dev
+call npm run dev
 pause
